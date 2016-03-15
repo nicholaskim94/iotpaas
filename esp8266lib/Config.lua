e39d5f147371c565ab269144cb5db9e236b8c0ea
KimDongjae <dong4810@hotmail.com>
2016-03-16 00:29:32 +0900

    json, url 셀렉터 추가

--- esp8266lib/Config.lua
+++ esp8266lib/Config.lua
@@ -1,10 +1,13 @@
 --GLOBAL VARIABLE
-SSID = "SSID" --Put Your Wifi SSID on Here!
-Pass = "PASSWORD" --Put Your Wifi Password on Here!
+SSID = "iptimenetis" --Put Your Wifi SSID on Here!
+Pass = "12345678" --Put Your Wifi Password on Here!
 TimeOut = 10000 --Connection Closed after Timor, Put Nil to Run 'connect_wifi.lua' until Wifi Connected!
 Host = node.chipid()  -- set Host
-IP = "" -- set your IP
-Path = "/post"
+IP = "127.0.0.1" -- set your IP
+PORT = 8080
+SECURITY = 0
+Path = "/index.html"
+EncodeType = "url" -- select json or url
 ReqType = "POST"
 UploadInterval = 10000 -- Unit: ms
 MuxSelPinArray = {0, 1, 2} --muxpin setting	0, 1, 2 for sel pin
@@ -24,7 +27,6 @@ DataContainer = {
     ["unit"] = "Celcius",
     ["time"] = "",
     ["value"] = 110,
-    ["lat"] = 0,
-    ["lng"] = 0,
+    ["location"] = "My Room",
 	["access_token"] = "16586e3d75a12ea0c560ed5045ee9af1" -- put your access_token
 }
--- esp8266lib/Request.lua
+++ esp8266lib/Request.lua
@@ -1,15 +1,16 @@
 function urlparser(t) local result = "" for k,v in pairs(t) do result = result.."&"..k.."="..v end return string.sub(result, 2) end
 function jsonparser(t) local result = "{" for k,v in pairs(t) do result = result..'"'..k..'":"'..v..'",' end return (string.sub(result,0,-2)..'}') end
+function selparser(sel, data) if (sel == "json") then return jsonparser(data) elseif (sel == "url") then return urlparser(data) else print ("EncodeType Error") end end
 function PostRequest(path, data)
     return "POST /"..path.." HTTP/1.1\r\n"..
-      Header..jsonparser(data).."/r/n/r/n"
+      Header..selparser(EncodeType ,data).."\r\n\r\n"
 end
 function GetRequest(path, data)
-    return "GET /"..path.."?"..urlparser(data).." HTTP/1.1\r\n"..Header.."/r/n"
+    return "GET /"..path.."?"..selparser(data).." HTTP/1.1\r\n"..Header.."\r\n"
 end
-conn=net.createConnection(net.TCP, 0) 
-conn:on("receive", function(sck, pl) end)
-conn:connect(80, IP)
+conn=net.createConnection(net.TCP, SECURITY) 
+conn:on("receive", function(sck, pl) print (pl) end)
+conn:connect(PORT, IP)
 if ReqType == "POST" then
 	conn:send(PostRequest(Path,DataContainer))
 elseif ReqType == "GET" then
